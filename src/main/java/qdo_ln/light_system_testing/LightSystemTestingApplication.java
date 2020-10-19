2d7817f3b5540e61fcef3dbeac52e52cf67ec531
1Zaytsev <4yu@bk.ru>
2020-10-20 00:58:56 +0300

    Initial commit

---
+++ src/main/java/qdo_ln/light_system_testing/LightSystemTestingApplication.java
@@ -0,0 +1,13 @@
+package qdo_ln.light_system_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightSystemTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightSystemTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qdo_ln/light_system_testing/LightSystemTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package qdo_ln.light_system_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightSystemTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
