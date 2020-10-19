46053ac3df46abaaf6ed31acbadf0b1fdd6967be
1Zaytsev <4yu@bk.ru>
2020-10-20 02:44:40 +0300

    Native query 'findAllByProjectId' to DeviceRepository added

--- src/main/java/qdo_ln/light_system_testing/repositories/DeviceRepository.java
+++ src/main/java/qdo_ln/light_system_testing/repositories/DeviceRepository.java
@@ -1,7 +1,21 @@
 package qdo_ln.light_system_testing.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import qdo_ln.light_system_testing.entities.Device;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 public interface DeviceRepository extends JpaRepository<Device, Integer> {
+
+    /**
+     * Поиск всех девайсов по идентификатору проекта
+     */
+    @Modifying
+    @Query(value = "SELECT id, serial_number FROM devices WHERE project_id = :projectId", nativeQuery = true)
+    @Transactional
+    List<Device> findAllByProjectId(@Param("projectId") int projectId);
 }
